f9aace69378a59de57c9a6f335790dfd1aa4acd7
Rabensteiner David <rabendavid02@gmail.com>
2023-11-04 23:07:14 +0100

    Cards at the bottom

--- App/lib/components/AnimatedMarkersMap.dart
+++ App/lib/components/AnimatedMarkersMap.dart
@@ -1,4 +1,6 @@
 import 'dart:typed_data';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:geocoding/geocoding.dart';
@@ -10,30 +12,13 @@ import 'package:latlong2/latlong.dart' as latlong;
 
 import '../shema/MapMarker.dart';
 
-var myCurrentLocation = const latlong.LatLng(48.36664, 14.516467);
-const myLocation = latlong.LatLng(48.36664, 14.516467);
-
-const myLocation2 = latlong.LatLng(48.36663, 14.516300);
-
-final List<MapMarker> mapMarkers = [
-  MapMarker(
-    image: 'assets/Marker.png',
-    title: 'Marcos',
-    address: 'Address Marcos 123',
-    location: myCurrentLocation,
-  ),
-  MapMarker(
-    image: 'assets/Marker.png',
-    title: 'Paavo',
-    address: 'Address Paavo 123',
-    location: myLocation2,
-  ),
-];
-
+var myCurrentLocation = latlong.LatLng(0, 0);
 
 const MAPBOX_ACCESS_TOKEN = 'sk.eyJ1IjoiYm9uaXRoYW4iLCJhIjoiY2xvaGFydjR1MGV5bDJqbnZ6cWg0dXh4cyJ9.m3uRWclpqOdSgYfUegOlTg';
 const MAPBOX_STYLE = 'mapbox://styles/bonithan/cloh3lx0f000d01qoh3okhz10';
 const MARKER_COLOR = Color (0xFF3DC5A7);
+const MARKER_SIZE_EXPAND = 55.0;
+const MARKER_SIZE_SHRINK = 30.0;
 
 class AnimatedMarkersMap extends StatefulWidget {
   const AnimatedMarkersMap({Key? key}) : super(key: key);
@@ -47,7 +32,74 @@ class _LocationPageState extends State<AnimatedMarkersMap> {
   Position? _currentPosition;
   MapboxMapController? mapController;
   double iconSize = 0.3;
- 
+  int _markerIndex = 0;
+
+  final PageController _pageController = PageController(initialPage: 0);
+
+  bool isCardVisible = false;
+
+
+
+  User? user = FirebaseAuth.instance.currentUser;
+  final firestore = FirebaseFirestore.instance;
+
+  List<MapMarker> mapMarkers = [
+    MapMarker(
+      image: 'assets/Marker.png',
+      title: 'Your Position',
+      address: 'Your adress',
+      location: myCurrentLocation, start: Timestamp.now(), end: Timestamp.now(), description: "he",
+    ),
+    /*  MapMarker(
+      image: 'assets/Marker.png',
+      title: 'Paavo',
+      address: 'Address Paavo 123',
+      location: myLocation2,
+    ),*/
+  ];
+
+  void getPositionActivity() async {
+    var userID = user?.uid;
+
+    if (userID != null) {
+      Timestamp now = Timestamp.now();
+      final activitiesCollectionRef = firestore.collection("activities");
+
+      activitiesCollectionRef.get().then((querySnapshot) {
+        querySnapshot.docs.forEach((activityDoc) {
+          final activityName = activityDoc.id;
+
+          if(activityName == "volleyball"){
+            final locationsCollectionRef = activityDoc.reference.collection("location");
+
+            locationsCollectionRef.get().then((locationQuerySnapshot) {
+              locationQuerySnapshot.docs.forEach((doc) {
+                final data = doc.data();
+                if (data != null) {
+                  final description = data["description"];
+                  final nameActivity = data["name_activity"];
+                  GeoPoint location = data["position"];
+
+                  mapMarkers.add(MapMarker(
+                    image: 'assets/Marker.png',
+                    title: nameActivity,
+                    address: 'Address Paavo 123',
+                    location: latlong.LatLng(location.latitude,location.longitude), start: now, end: now, description: description,
+                  ));
+
+                } else {
+                  print("Field not found in data.");
+                }
+                print("Aktivität: $activityName, Standortdaten: $data");
+              });
+            });
+          }
+        });
+      });
+    } else {
+      print("User ID is null. Make sure the user is authenticated.");
+    }
+  }
 
   Future<bool> _handleLocationPermission() async {
     bool serviceEnabled;
@@ -91,6 +143,8 @@ class _LocationPageState extends State<AnimatedMarkersMap> {
     }).catchError((e) {
       debugPrint(e.toString());
     });
+
+
   }
 
   Future<void> _getAddressFromLatLng(Position position) async {
@@ -107,6 +161,7 @@ class _LocationPageState extends State<AnimatedMarkersMap> {
     });
   }
 
+
   List<Marker> _buildMarkers() {
     final List<Marker> markerList = [];
 
@@ -115,13 +170,26 @@ class _LocationPageState extends State<AnimatedMarkersMap> {
 
       markerList.add(
         Marker(
+          height: MARKER_SIZE_EXPAND,
+          width: MARKER_SIZE_EXPAND,
           point: mapItem.location,
           builder: (_) {
             return GestureDetector(
               onTap: () {
+                setState(() {
+                  isCardVisible = true; // Show the card section
+                  _markerIndex = i;
+                });
                 print('Selected: ${mapItem.title}');
+                if (_pageController.hasClients){
+                  _pageController.animateToPage(
+                    i,
+                    duration: const Duration(milliseconds: 500),
+                    curve: Curves.ease,
+                  );
+                }
               },
-              child: Image.asset("assets/Marker.png"),
+              child: _LocationMarker(selected: _markerIndex == i),
             );
           },
         ),
@@ -130,11 +198,22 @@ class _LocationPageState extends State<AnimatedMarkersMap> {
     return markerList;
   }
 
-
   @override
   void initState() {
     super.initState();
+    // Fetch initial data
+    getPositionActivity();
     _getCurrentPosition();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  void onClick(){
+
   }
 
   @override
@@ -148,46 +227,117 @@ class _LocationPageState extends State<AnimatedMarkersMap> {
       ),
       body: Stack(
         children: [
-          FlutterMap(
-            options: MapOptions(
-              minZoom: 5,
-              maxZoom: 22,
-              zoom: 18,
-              center: myCurrentLocation,
-            ),
-            children: [
-              TileLayer(
-                urlTemplate:
-                'https://api.mapbox.com/styles/v1/{style_id}/tiles/{z}/{x}/{y}?access_token={access_token}',
-                additionalOptions: const {
-                  'style_id': 'bonithan/cloh3lx0f000d01qoh3okhz10',
-                  'access_token': MAPBOX_ACCESS_TOKEN,
-                },
+          GestureDetector(
+            child: _currentPosition != null
+                ? FlutterMap(
+              options: MapOptions(
+                onTap: (tapPosition, point){
+                  if(tapPosition != Marker){
+                    setState(() {
+                      isCardVisible = false; // Hide the card section
+                    });
+                  }
+              },
+                minZoom: 1,
+                maxZoom: 19,
+                zoom: 19,
+                center: myCurrentLocation,
               ),
-              MarkerLayer(
-                markers: _buildMarkers(),
-              ),
-            ],
+              children: [
+                TileLayer(
+                  maxZoom: 19,
+                  urlTemplate:
+                  'https://api.mapbox.com/styles/v1/{style_id}/tiles/{z}/{x}/{y}?access_token={access_token}',
+                  additionalOptions: const {
+                    'style_id': 'bonithan/cloh3lx0f000d01qoh3okhz10',
+                    'access_token': MAPBOX_ACCESS_TOKEN,
+                  },
+                ),
+
+                MarkerLayer(
+                  markers: _buildMarkers(),
+                ),
+              ],
+            ) : const Center(child: CircularProgressIndicator()),
           ),
+
           Positioned(
-            bottom: 0,
+            bottom: 30,
             left: 0,
             right: 0,
-            child: Container(
-              color: Colors.white,
-              padding: const EdgeInsets.all(46.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('LAT: ${_currentPosition?.latitude ?? ""}'),
-                  Text('LNG: ${_currentPosition?.longitude ?? ""}'),
-                  Text('ADDRESS: ${_currentAddress ?? ""}'),
-                ],
-              ),
+            height: MediaQuery.of(context).size.height * 0.3,
+            child: Visibility(
+              visible: isCardVisible,
+              child: PageView.builder(
+                controller: _pageController,
+                itemCount: mapMarkers.length,
+                itemBuilder: (context, index) {
+                  final item = mapMarkers[index];
+                  return _MapItemDetails(
+                    mapMarker: item,
+                  );
+                },
+              )
             ),
-          ),
+          )
         ],
       ),
+    );
+  }
+}
+
+
+
+class _MapItemDetails extends StatelessWidget {
+   const _MapItemDetails( {
+    Key? key,
+    required this.mapMarker,
+  }) : super(key: key);
+
+  final MapMarker mapMarker;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.white,
+      margin: const EdgeInsets.all(16.0),
+      child: Align(
+        alignment: Alignment.center,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                mapMarker.image,
+                width: 100, // Adjust the width to your needs
+                height: 100, // Adjust the height to your needs
+              ),
+              Text(mapMarker.title),
+              Text(mapMarker.description),
+              Text(mapMarker.address),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _LocationMarker extends StatelessWidget {
+  const _LocationMarker({Key? key, this.selected = false}) : super(key: key);
+  final bool selected;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = selected ? MARKER_SIZE_EXPAND : MARKER_SIZE_SHRINK;
+    return Center(
+      child: AnimatedContainer(
+        height: size, width: size,
+        duration: const Duration(milliseconds: 400),
+        child: Image.asset('assets/Marker.png'),
+      ),
+      // AnimatedContainer
     );
   }
 }
--- App/lib/shema/MapMarker.dart
+++ App/lib/shema/MapMarker.dart
@@ -1,14 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:latlong2/latlong.dart'; // Import LatLng from 'latlong2' package
 
 class MapMarker {
   final String image;
   final String title;
+  final String description;
+  final Timestamp start;
+  final Timestamp end;
   final String address;
   final LatLng location; // Assuming LatLng is from the 'latlong2' package
 
   MapMarker({
     required this.image,
     required this.title,
+    required this.start,
+    required this.end,
+    required this.description,
     required this.address,
     required this.location,
   });
