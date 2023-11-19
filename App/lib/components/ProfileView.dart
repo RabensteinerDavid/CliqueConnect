d807b4f6cd535a315d29620ebad478f2b8e523ce
julsRoll <s2210238053@fhooe.at>
2023-11-19 23:44:25 +0100

    profileview edited

--- App/lib/components/ProfileView.dart
+++ App/lib/components/ProfileView.dart
@@ -3,6 +3,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
+
+import '../main.dart';
+
 final filters = <String>{};
 User? user = FirebaseAuth.instance.currentUser;
 
@@ -18,7 +21,9 @@ class _YourCurrentScreenState extends State<ProfileView> {
 
   final firestore = FirebaseFirestore.instance;
   static String imageURL = '';
+  late Future<String> userNameFuture;
   late Future<String> courseFuture;
+  late Future<String> universityFuture;
   late Future<String> aboutMeFuture;
   late Future<String> interestsFuture;
   late Future<bool> imageUrlFuture;
@@ -28,7 +33,9 @@ class _YourCurrentScreenState extends State<ProfileView> {
   void initState() {
     super.initState();
     imageUrlFuture = getImgUrl();
+    userNameFuture = getUserName();
     courseFuture = getCourse();
+    universityFuture = getUniversity();
     aboutMeFuture = getAboutMeText();
     interestsFuture = getInterests();
   }
@@ -36,85 +43,175 @@ class _YourCurrentScreenState extends State<ProfileView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Your Current Screen'),
-      ),
-      body: Center(
-        child: FutureBuilder(
-          // Use a List or Tuple to combine multiple futures
-          future: Future.wait([courseFuture, aboutMeFuture, interestsFuture, imageUrlFuture]),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              if (snapshot.hasData) {
-                // Extract data from the snapshot
-                var course = snapshot.data![0];
-                var aboutMe = snapshot.data![1];
-                var interests = snapshot.data![2] as String; // Change this line
-                var imageUrl = snapshot.data![3];
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            color: MyApp.blueMain,
+          ),
+          Positioned(
+            top: 80, // Adjust the top position as needed
+            left: 0,
+            right: 0,
+            child: Align(
+              alignment: Alignment.center,
+              child: CircleAvatar(
+                radius: 55,
+                backgroundColor: const Color(0xff8179b4),
+                child: ClipOval(
+                  child: imageURL.isNotEmpty
+                      ? Image.network(
+                    imageURL,
+                    width: 100,
+                    height: 100,
+                    fit: BoxFit.cover,
+                  )
+                      : Image.asset(
+                    'assets/cliqueConnect.png',
+                    width: 100,
+                    height: 100,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height * 0.2, // Adjust the top position as needed
+            left: 0,
+            right: 0,
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.8,
+                width: MediaQuery.of(context).size.width,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0),
+                  ),
+                ),
+                child: FutureBuilder(
+                  // Use a List or Tuple to combine multiple futures
+                  future: Future.wait([courseFuture, universityFuture, aboutMeFuture, interestsFuture, imageUrlFuture, userNameFuture]),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      if (snapshot.hasData) {
+                        // Extract data from the snapshot
+                        var course = snapshot.data![0];
+                        var university = snapshot.data![1];
+                        var aboutMe = snapshot.data![2];
+                        var interests = snapshot.data![3] as String; // Change this line
+                        var imageUrl = snapshot.data![4];
+                        var username = snapshot.data![5];
 
-                // Display the course and aboutMe in a nice way
-                return Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CircleAvatar(
-                      radius: 55,
-                      backgroundColor: const Color(0xff8179b4),
-                      child: ClipOval(
-                        child: imageURL.isNotEmpty
-                            ? Image.network(
-                          imageURL,
-                          width: 100,
-                          height: 100,
-                          fit: BoxFit.cover,
-                        )
-                            : Image.asset(
-                          'assets/cliqueConnect.png',
-                          width: 100,
-                          height: 100,
-                          fit: BoxFit.cover,
+                        return Padding (
+                            padding: EdgeInsets.only(left: 20), // Adjust the left padding as needed
+                    child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(height: 60),
+                             Text(
+                              '$username',
+                              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                            const SizedBox(height: 16),
+                            Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    Image.asset(
+                                      'icons/hat_rose.png',
+                                      width: 40,
+                                      height: 40,
+                                    ),
+                                    const SizedBox(width: 20), // Adjust the spacing as needed
+                                    Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          '$course',
+                                          style: const TextStyle(
+                                            fontSize: 18,
+                                            color: MyApp.blueMain,
+                                          ),
+                                        ),
+                                        Text(
+                                          '$university',
+                                          style: TextStyle(fontSize: 16),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: 20), // Adjust the vertical spacing between rows
+                                Row(
+                                  children: [
+                                    Image.asset(
+                                      'icons/profile_rose.png',
+                                      width: 40,
+                                      height: 40,
+                                    ),
+                                    const SizedBox(width: 20), // Adjust the spacing as needed
+                                    Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        const Text(
+                                          'About Me',
+                                          style: TextStyle(
+                                            fontSize: 18,
+                                            color: MyApp.blueMain,
+                                          ),
+                                        ),
+                                        Text(
+                                          '$aboutMe',
+                                          style: TextStyle(fontSize: 16),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: 20),
+                                Row(
+                                  children: [
+                                    Text(
+                                      'Interests: $interests',
+                                      style: TextStyle(fontSize: 16),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    const Text(
-                      'User Name',
-                      style: TextStyle(
-                          fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 16),
-                    Text(
-                      'Course: $course',
-                      style: TextStyle(fontSize: 16),
-                    ),
-
-                    Text(
-                      'About Me: $aboutMe',
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    Text(
-                      'Interests: $interests',
-                      style: TextStyle(fontSize: 16),
-                    ),
-                  ],
-                );
-              } else {
-                // Handle the case where data is not available
-                return const Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('Data not available.'),
-                  ],
-                );
-              }
-            } else {
-              // Show a loading indicator while waiting for the Futures to complete.
-              return const CircularProgressIndicator();
-            }
-          },
-        ),
+                        );
+                      } else {
+                        // Handle the case where data is not available
+                        return const Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('Data not available.'),
+                          ],
+                        );
+                      }
+                    } else {
+                      // Show a loading indicator while waiting for the Futures to complete.
+                      return const CircularProgressIndicator();
+                    }
+                  },
+                ),
+              ),
+            ),
+          ), ],
       ),
     );
   }
+
+
+
+
 
   Future<bool> getImgUrl() async {
     var userID = user?.uid;
@@ -163,6 +260,26 @@ class _YourCurrentScreenState extends State<ProfileView> {
     return false;
   }
 
+  Future<String> getUserName() async {
+    var userID = user?.uid;
+    var userName = "No Username Available";
+
+    if (userID != null) {
+      final snapshot = await firestore.collection("users").doc(userID).get();
+
+      if (snapshot.exists) {
+        final data = snapshot.data() as Map<String, dynamic>;
+        final name = data["username"];
+
+        print(name);
+        if (name != null) {
+          userName = name;
+        }
+      }
+    }
+    return userName;
+  }
+
   Future<String> getCourse() async {
     var userID = user?.uid;
     var course = "No Course Available";
@@ -181,6 +298,26 @@ class _YourCurrentScreenState extends State<ProfileView> {
       }
     }
     return course;
+  }
+
+  Future<String> getUniversity() async {
+    var userID = user?.uid;
+    var university = "No University Available";
+
+    if (userID != null) {
+      final snapshot = await firestore.collection("users").doc(userID).get();
+
+      if (snapshot.exists) {
+        final data = snapshot.data() as Map<String, dynamic>;
+        final userUniversity = data["universityType"];
+
+        print(userUniversity);
+        if (userUniversity != null) {
+          university = userUniversity;
+        }
+      }
+    }
+    return university;
   }
 
   Future<String> getAboutMeText() async {
