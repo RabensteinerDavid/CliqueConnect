8189064018194c320944853078b264d592e232a0
annaunterlerchner <annaunterlerchner@gmail.com>
2023-12-07 18:49:03 +0100

    calendar update

--- App/lib/components/Calendar.dart
+++ App/lib/components/Calendar.dart
@@ -1,133 +1,187 @@
-// ignore_for_file: unused_field
+  import 'package:flutter/material.dart';
+  import 'package:flutter_neat_and_clean_calendar/flutter_neat_and_clean_calendar.dart';
+  import 'package:cloud_firestore/cloud_firestore.dart';
 
-import 'package:flutter/material.dart';
-import 'package:flutter_neat_and_clean_calendar/flutter_neat_and_clean_calendar.dart';
+  import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+import 'package:rrule/rrule.dart';
 
-class CalendarScreen extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _CalendarScreenState();
-  }
-}
 
-class _CalendarScreenState extends State<CalendarScreen> {
-  bool showEvents = true;
+  import '../main.dart';
+  import '../models/user.dart';
 
-  List<NeatCleanCalendarEvent> _todaysEvents = [
-    NeatCleanCalendarEvent(
-      'Event A',
-      startTime: DateTime(
-          DateTime.now().year, DateTime.now().month, DateTime.now().day, 10, 0),
-      endTime: DateTime(
-          DateTime.now().year, DateTime.now().month, DateTime.now().day, 12, 0),
-      description: 'A special event',
-      color: Colors.blue[700],
-    ),
-  ];
 
-  final List<NeatCleanCalendarEvent> _eventList = [
-    NeatCleanCalendarEvent(
-      'MultiDay Event A',
-      description: 'test desc',
-      startTime: DateTime(
-          DateTime.now().year, DateTime.now().month, DateTime.now().day, 10, 0),
-      endTime: DateTime(DateTime.now().year, DateTime.now().month,
-          DateTime.now().day + 2, 12, 0),
-      color: Colors.orange,
-      isMultiDay: true,
-    ),
-    NeatCleanCalendarEvent(
-        'Allday Event B',
-        description: 'test desc',
-        startTime: DateTime(DateTime.now().year, DateTime.now().month,
-            DateTime.now().day - 2, 14, 30),
-        endTime: DateTime(DateTime.now().year, DateTime.now().month,
-            DateTime.now().day + 2, 17, 0),
-        color: Colors.pink,
-        isAllDay: true,
-        icon: 'assets/cliqueConnect.png',
-    ),
-    NeatCleanCalendarEvent(
-      'Normal Event D',
-      description: 'test desc',
-      startTime: DateTime(DateTime.now().year, DateTime.now().month,
-          DateTime.now().day, 14, 30),
-      endTime: DateTime(
-          DateTime.now().year, DateTime.now().month, DateTime.now().day, 17, 0),
-      color: Colors.indigo,
-      icon: 'assets/cliqueConnect.png',
-    ),
-    NeatCleanCalendarEvent(
-      'Normal Event E',
-      description: 'test desc',
-      startTime: DateTime(
-          DateTime.now().year, DateTime.now().month, DateTime.now().day, 7, 45),
-      endTime: DateTime(
-          DateTime.now().year, DateTime.now().month, DateTime.now().day, 9, 0),
-      color: Colors.indigo,
-      icon: 'assets/cliqueConnect.png',
-    ),
-  ];
 
-  @override
-  void initState() {
-    super.initState();
-    // Force selection of today on first load, so that the list of today's events gets shown.
-    _handleNewDate(DateTime(
-        DateTime.now().year, DateTime.now().month, DateTime.now().day));
+
+  class CalendarScreen extends StatefulWidget {
+    @override
+    State<StatefulWidget> createState() {
+      return _CalendarScreenState();
+    }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Calendar(
-          startOnMonday: true,
-          weekDays: ['Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa', 'So'],
-          eventsList: _eventList,
-          isExpandable: true,
-          eventDoneColor: Colors.green,
-          selectedColor: Colors.pink,
-          selectedTodayColor: Colors.green,
-          todayColor: Colors.blue,
-          eventColor: null,
-          locale: 'de_DE',
-          todayButtonText: 'Heute',
-          allDayEventText: 'Ganztägig',
-          multiDayEndText: 'Ende',
-          isExpanded: true,
-          expandableDateFormat: 'EEEE, dd. MMMM yyyy',
-          onEventSelected: (value) {
-            print('Event selected ${value.summary}');
-          },
-          onEventLongPressed: (value) {
-            print('Event long pressed ${value.summary}');
-          },
-          onMonthChanged: (value) {
-            print('Month changed $value');
-          },
-          onRangeSelected: (value) {
-            print('Range selected ${value.from} - ${value.to}');
-          },
-          datePickerType: DatePickerType.date,
-          dayOfWeekStyle: TextStyle(
-              color: Colors.black, fontWeight: FontWeight.w800, fontSize: 11),
-          showEvents: showEvents,
+  class _CalendarScreenState extends State<CalendarScreen> {
+    bool showEvents = true;
+    late String nameActivityCalender;
+
+    List<NeatCleanCalendarEvent> _eventList = [];
+
+
+    @override
+    void initState() {
+      super.initState();
+      _initializeData();
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        body: SafeArea(
+          child: Calendar(
+            startOnMonday: true,
+            weekDays: ['MO', 'TU', 'WE', 'TH', 'FR', 'SA', 'SU'],
+            eventsList: _eventList,
+            isExpandable: true,
+            eventDoneColor: const Color(0xFFACA9BF),
+            selectedColor: const Color(0xFF6059F0),
+            selectedTodayColor: Colors.green,
+            todayColor: Colors.blue,
+            eventColor: null,
+            locale: 'de_DE',
+            todayButtonText: 'Heute',
+            allDayEventText: 'Ganztägig',
+            multiDayEndText: 'Ende',
+            isExpanded: true,
+            expandableDateFormat: 'EEEE, dd. MMMM yyyy',
+            onEventSelected: (value) {
+              print('Event selected ${value.summary}');
+            },
+            onEventLongPressed: (value) {
+              print('Event long pressed ${value.summary}');
+            },
+            onMonthChanged: (value) {
+              print('Month changed $value');
+            },
+            onRangeSelected: (value) {
+              print('Range selected ${value.from} - ${value.to}');
+            },
+            datePickerType: DatePickerType.date,
+            dayOfWeekStyle: TextStyle(
+                color: Colors.black, fontWeight: FontWeight.w800, fontSize: 11),
+            showEvents: showEvents,
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            showEvents = !showEvents;
-          });
-        },
-        child: Icon(showEvents ? Icons.visibility_off : Icons.visibility),
-        backgroundColor: Colors.green,
-      ),
-    );
-  }
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              showEvents = !showEvents;
+            });
+          },
+          child: Icon(showEvents ? Icons.visibility_off : Icons.visibility),
+          backgroundColor: Colors.green,
+        ),
+      );
+    }
 
-  void _handleNewDate(date) {
-    print('Date selected: $date');
+
+
+
+
+    Future<void> _initializeData() async {
+      List<EventData> eventDataList = await getMarkersAsFuture();
+
+      setState(() {
+        _eventList = convertToCalendarEvents(eventDataList);
+      });
+    }
+
+
+
+    Future<List<EventData>> getMarkersAsFuture() async {
+      firebase_auth.User? user = firebase_auth.FirebaseAuth.instance.currentUser;
+      final firestore = FirebaseFirestore.instance;
+      final userID = user?.uid;
+      List<EventData> eventDataList = [];
+      List<dynamic> alldata =[];
+
+      if (userID != null) {
+        final activitiesCollectionRef = firestore.collection("activities");
+
+        final querySnapshot = await activitiesCollectionRef.get();
+
+        for (final activityDoc in querySnapshot.docs) {
+          final Map<String, dynamic> data = activityDoc.data();
+          for (final key in data.keys) {
+            alldata = List.from(data[key]);
+            final nameActivity = await alldata[0];
+            nameActivityCalender = nameActivity;
+
+            final startTimestamp = await alldata[2]; // Assuming this is a Timestamp object
+            final start = startTimestamp.seconds * 1000 + (startTimestamp.nanoseconds / 1e6).round();
+
+            final description = await alldata[1];
+            final location = await alldata[4];
+            final category = await alldata[6];
+            var ruleNew = "";
+            var rrule;
+            if (alldata.length > 8) {
+              ruleNew = await alldata[8];
+              rrule = RecurrenceRule.fromString(ruleNew);
+            }
+            print(rrule);
+            eventDataList.add(EventData(
+              title: nameActivity,
+              startTime: DateTime.fromMillisecondsSinceEpoch(start),
+              endTime: DateTime.fromMillisecondsSinceEpoch(start),
+              description: description,
+              location: location,
+              category: category,
+              rrule: rrule,
+            ));
+          }
+        }
+
+        return eventDataList;
+      } else {
+        return eventDataList;
+      }
+    }
+
+
+
+    // Funktion zum Umwandeln von Datenbankdaten in das Event-Format
+    List<NeatCleanCalendarEvent> convertToCalendarEvents(List<EventData> events) {
+      List<NeatCleanCalendarEvent> calendarEvents = [];
+
+      for (EventData event in events) {
+        calendarEvents.add(NeatCleanCalendarEvent(
+          event.title,
+          startTime: event.startTime,
+          endTime: event.endTime,
+          description: event.description,
+          //color: event.color,
+          //isMultiDay: event.isMultiDay,
+          //isAllDay: event.isAllDay,
+          //icon: event.icon,
+        ));
+      }
+
+      return calendarEvents;
+    }
+
   }
-}
+  class EventData {
+    final String title;
+    final DateTime startTime;
+    final DateTime endTime;
+    final String description;
+
+    // ... (andere Event-Attribute)
+
+    EventData({
+      required this.title,
+      required this.startTime,
+      required this.endTime,
+      required this.description, required location, required category, required rrule,
+      // ... (andere Event-Attribute)
+    });
+  }
